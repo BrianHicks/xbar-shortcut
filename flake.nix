9ebab4b1b11bd7b6601991ed311230e35c8c51ff
Brian Hicks <brian@brianthicks.com>
2024-07-12 17:32:08 -0500

    package for use with Nix

--- flake.nix
+++ flake.nix
@@ -1,38 +1,59 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    naersk.url = "github:nmattia/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import inputs.nixpkgs { inherit system; };
-
+        pkgs = nixpkgs.legacyPackages."${system}";
+        naersk-lib = naersk.lib."${system}";
         darwinInputs =
           if pkgs.stdenv.isDarwin then
             [
-              pkgs.darwin.apple_sdk.frameworks.Security
               pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
             ]
           else
             [ ];
       in
-      {
+      rec {
         formatter = pkgs.nixpkgs-fmt;
 
-        devShell = pkgs.mkShell {
-          packages = [
-            pkgs.rustc
-            pkgs.cargo
-            pkgs.clippy
-            pkgs.rustfmt
-            pkgs.rust-analyzer
-
-            # macOS needs this for whatever reason
+        # `nix build`
+        packages.xbar-shortcut = naersk-lib.buildPackage {
+          root = ./.;
+          buildInputs = [
             pkgs.libiconv
+            pkgs.openssl
+            pkgs.pkg-config
+            pkgs.rustPackages.clippy
           ] ++ darwinInputs;
+
+          doCheck = true;
+          checkPhase = ''
+            cargo clippy -- --deny warnings
+          '';
         };
-      }
-    );
+        defaultPackage = packages.xbar-shortcut;
+        overlay = final: prev: { xbar-shortcut = packages.xbar-shortcut; };
+
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          nativeBuildInputs =
+            [
+              pkgs.cargo
+              pkgs.cargo-edit
+              pkgs.rust-analyzer
+              pkgs.clippy
+              pkgs.rustc
+              pkgs.rustfmt
+
+              # for some reason this seems to be required, especially on macOS
+              pkgs.clippy
+            ] ++ darwinInputs;
+        };
+      });
 }
