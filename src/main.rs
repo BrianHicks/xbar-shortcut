65e7bfcd4de9e5b8009ce580a1dab9b36294452e
Brian Hicks <brian@brianthicks.com>
2024-07-12 14:10:48 -0500

    add shortcut_token argument

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use clap::Parser;
 
 #[derive(Debug, Parser)]
-struct Opts {}
+struct Opts {
+    /// The token to use to request information from Shortcut's REST API
+    shortcut_token: String,
+}
 
 fn main() {
     let opts = Opts::parse();
